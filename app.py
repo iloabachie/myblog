092644c546f7d14e82321c71987e04f171342480
Udemezue Iloabachie <udemezue@gmail.com>
2024-06-10 20:17:21 -0400

    day 3 - error handling

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ from os import getenv
 # Create a Flask Instance
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 # Create a route decorator
 @app.route('/')
 def index():
