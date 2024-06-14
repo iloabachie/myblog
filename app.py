db3ced7a2b966fafa20f4fb2a0fd822deaa1ecc5
Udemezue Iloabachie <udemezue@gmail.com>
2024-06-13 20:42:17 -0400

    add forms -wtf

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
 from os import getenv
-
-
+from flask import Flask, render_template
+from secrets import token_urlsafe
+# from flask_bootstrap import Bootstrap5
+from flask_wtf import CSRFProtect, FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+
+foo = token_urlsafe(16)
+app.secret_key = foo
+
+# bootstrap = Bootstrap5(app)
+csrf = CSRFProtect(app)
+
+# app.config['SECRET_KEY'] = foo 
+class NamerForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -23,6 +39,24 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
